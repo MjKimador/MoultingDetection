f1118bfa994a73bce8c20c2644c284fb2ab1636c
MjKimador <sithomolalutador@gmail.com>
2025-05-18 17:38:56 +0200

    Updated db url

--- app/database.py
+++ app/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 
-DB_URL = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
+DB_URL = (
+    f"mysql+pymysql://{os.getenv('MYSQL_USER')}:"
+    f"{os.getenv('MYSQL_PASSWORD')}@"
+    f"{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/"
+    f"{os.getenv('MYSQL_DB')}"
+)
+
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
